8d7243fc04368790443f40b3817aa47329ca4f36
Joshua JJ <akhidenorjoshua@gmail.com>
2023-04-26 14:31:34 +0100

    Add login and sign up functionalities

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -8,16 +8,30 @@ const { BadRequestError, AuthorizationError } = require('../errors');
 
 const signUp = async (req, res) => {
   const { email, password, confirmPassword } = req.body;
+  const regex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+
   if (!email || !password || !confirmPassword) {
     throw new BadRequestError('Please provide all fields.');
+  }
+
+  if (!regex.test(email)) {
+    throw new BadRequestError('Please provide a valid email');
   }
 
   let queryOperator = `SELECT * FROM operators where email='${email}'`;
   let [result] = await db.query(queryOperator);
 
   if (result.length == 1) throw new BadRequestError('Email already exists.');
+
+  if (password !== confirmPassword)
+    throw new BadRequestError("Passwords don't match.");
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  let queryInsertOperator = `INSERT INTO operators (email, password) VALUES ('${email}', '${hashedPassword}')`;
+
+  await db.query(queryInsertOperator);
   res.status(StatusCodes.CREATED).json({ mssg: 'User created' });
-}
+};
 
 const completeProfile = async (req, res) => {
   // const { firstName, lastName, username, email, password, confirmPassword } = req.body;
@@ -48,27 +62,25 @@ const completeProfile = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ mssg: 'Profile verified' });
 };
 
-
 // Function to log in users
 
 const login = async (req, res) => {
-  const { username, password } = req.body;
-  // if (!username || !password)
-  //   throw new BadRequestError('Please provide username and password');
+  const { email, password } = req.body;
+  if (!email || !password)
+    throw new BadRequestError('Please provide email and password');
 
-  // let query = `SELECT * FROM users WHERE username='${username}'`;
-  // const [[result]] = await db.query(query);
-  // if (!result) throw new AuthorizationError('Invalid username or password');
+  let query = `SELECT * FROM operators WHERE email='${email}'`;
+  const [[result]] = await db.query(query);
+  if (!result) throw new AuthorizationError('Invalid username or password');
 
-  // let match = await bcrypt.compare(password, result.password);
+  let match = await bcrypt.compare(password, result.password);
 
-  // if (!match) throw new AuthorizationError('Invalid username or password');
-  // const { id: userID, first_name: firstName } = result;
-  // const token = jwt.sign({ userID, firstName }, process.env.ACCESS_TOKEN_SECRET, {
-  //   expiresIn: '60d',
-  // });
-  // res.status(StatusCodes.OK).json({ mssg: result, token });
-  res.status(StatusCodes.OK).json({ mssg: 'Logged in' });
+  if (!match) throw new AuthorizationError('Invalid username or password');
+  const { id: operatorId } = result;
+  const token = jwt.sign({ operatorId, email }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: '60d',
+  });
+  res.status(StatusCodes.OK).json({ mssg: `${email} logged in successfully`, token });
 };
 
 module.exports = { signUp, completeProfile, login };
