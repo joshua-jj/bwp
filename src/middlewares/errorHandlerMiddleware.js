0b84e897dda59f85aefd45fc2b91b26238c4dbde
Joshua JJ <akhidenorjoshua@gmail.com>
2023-05-08 11:36:36 +0100

    Refactor code

--- src/middlewares/errorHandlerMiddleware.js
+++ src/middlewares/errorHandlerMiddleware.js
@@ -1,12 +1,14 @@
-const {CustomAPIError} = require('../errors');
+const { CustomAPIError } = require('../errors');
 const { StatusCodes } = require('http-status-codes');
 
 const errorHandlerMiddleware = (err, req, res, next) => {
   if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ mssg: err.message });
   }
-  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ mssg: 'Something went wrong, try again later' });
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
-}
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+  return res
+    .status(StatusCodes.INTERNAL_SERVER_ERROR)
+    .json({ mssg: 'Something went wrong, try again later' });
+};
 
 module.exports = errorHandlerMiddleware;
